0778041106d3b805492acaf79d552bf40cebc8ae
Ishaan Ahuja <59371101+ishaan2908@users.noreply.github.com>
2020-09-02 23:28:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,17 +32,17 @@ client.reload = command => {
 			delete require.cache[require.resolve(`./commands/${command}`)];
 			let cmd = require(`./commands/${command}`);
 			client.commands.delete(command);
-	      	client.aliases.forEach((cmd, alias) => {
-	        	if (cmd === command) client.aliases.delete(alias);
-	      	});
-		    client.commands.set(command, cmd);
-		    cmd.conf.aliases.forEach(alias => {
-		      	client.aliases.set(alias, cmd.help.name);
-	      	});
-	      	resolve();
-	    } catch (e)	{
-	      reject(e);
-	    }
+			client.aliases.forEach((cmd, alias) => {
+				if (cmd === command) client.aliases.delete(alias);
+			});
+			client.commands.set(command, cmd);
+			cmd.conf.aliases.forEach(alias => {
+				client.aliases.set(alias, cmd.help.name);
+			});
+			resolve();
+		} catch (e) {
+			reject(e);
+		}
 	});
 };
 
